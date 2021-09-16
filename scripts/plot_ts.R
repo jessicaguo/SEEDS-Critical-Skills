55000435f8663be6d2ce6b65cb8a13c1986534cf
Carla López Lloreda <carla.lpez09@gmail.com>
2021-09-16 16:57:09 -0400

    Improve readibility in script

--- scripts/plot_ts.R
+++ scripts/plot_ts.R
@@ -1,24 +1,43 @@
-#Manual data
-d<-read.csv("wp.csv")
+# Code for practicing style and best practices
+# By Riemer and Guo, modified by López Lloreda
+# 9/16/2021
+
+
+# Load libraries
+library(ggplot2)
+library(dplyr)
+
+#### Read in data ####
+
+# Manual data
+d <- read.csv("wp.csv")
+
+# Look at the structure of the data
 str(d)
 
-library(dplyr)
-SE<-function(x){sd(x,na.rm=T)/sqrt(length(x))}
-sum_WP<-d%>%
+# Create functions
+
+SE <- function(x){
+        sd(x,na.rm=T)/sqrt(length(x))
+}
+
+sum_WP <- d %>%
   mutate(datetime=as.POSIXct(strptime(datetime, "%m/%d/%Y %H:%M")),
          date=as.POSIXct(paste0(date," 00:00"), format="%m/%d/%Y %H:%M"))%>%
-  group_by(date)%>%
+  group_by(date) %>%
   summarize(m=mean(negWP,na.rm=T),se=SE(negWP),sd=sd(negWP))
 
 
-#Automated data
+# Automated data
 load("../psychrometer_append/clean3/new_1b_3.r")
+
 str(new_d1)
 
-library(ggplot2)
-fig1<-ggplot()+
-  geom_point(data=new_d1, aes(x=datetime, y=psy))+
-  geom_point(data=sum_WP, aes(x=date, y=m), stat="identity", size=3, col="red")+
-  geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
+#### Create plots ####
+
+fig1 <- ggplot() +
+          geom_point(data=new_d1, aes(x=datetime, y=psy))+
+          geom_point(data=sum_WP, aes(x=date, y=m), stat="identity", size=3, col="red")+
+          geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
 fig1
 
