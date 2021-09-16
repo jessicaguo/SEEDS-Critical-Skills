ab0d72541322bb04bd3ef16fabc6a4937ff2e036
Isa Siles <isa.sias151@gmail.com>
2021-09-16 15:57:48 -0500

    Improve comments in script

--- scripts/plot_ts.R
+++ scripts/plot_ts.R
@@ -1,8 +1,18 @@
-#Manual data
+#Styling an R code
+#modified by Isabel Siles
+
+#### Adding all the libraries ####
+
+library(dplyr)
+library(ggplot2)
+
+#### Manual data ####
+
+#adding the data
 d<-read.csv("wp.csv")
 str(d)
 
-library(dplyr)
+#Creating a function 
 SE<-function(x){sd(x,na.rm=T)/sqrt(length(x))}
 sum_WP<-d%>%
   mutate(datetime=as.POSIXct(strptime(datetime, "%m/%d/%Y %H:%M")),
@@ -10,15 +20,15 @@ sum_WP<-d%>%
   group_by(date)%>%
   summarize(m=mean(negWP,na.rm=T),se=SE(negWP),sd=sd(negWP))
 
+#### Automated data  ####
 
-#Automated data
+#adding the data
 load("../psychrometer_append/clean3/new_1b_3.r")
 str(new_d1)
 
-library(ggplot2)
+#creating a figure with the data
 fig1<-ggplot()+
   geom_point(data=new_d1, aes(x=datetime, y=psy))+
   geom_point(data=sum_WP, aes(x=date, y=m), stat="identity", size=3, col="red")+
   geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
 fig1
-
