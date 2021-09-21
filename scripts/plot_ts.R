0973e79a990de0b347af3966ab0b5b84afd7278b
Beth Avera <bethany.avera@gmail.com>
2021-09-21 13:18:44 -0600

    Cleaned more code

--- scripts/plot_ts.R
+++ scripts/plot_ts.R
@@ -17,17 +17,20 @@ library(dplyr)
 library(ggplot2)
 
 #### running code ####
-SE<-function(x){sd(x,na.rm=T)/sqrt(length(x))}
-sum_WP<-d%>%
-  mutate(datetime=as.POSIXct(strptime(datetime, "%m/%d/%Y %H:%M")),
-         date=as.POSIXct(paste0(date," 00:00"), format="%m/%d/%Y %H:%M"))%>%
-  group_by(date)%>%
-  summarize(m=mean(negWP,na.rm=T),se=SE(negWP),sd=sd(negWP))
+SE<-function(x){
+   sd(x, na.rm = TRUE) / sqrt(length(x))
+}
+
+sum_WP <- d %>%
+  mutate(datetime = as.POSIXct(strptime(datetime, "%m/%d/%Y %H:%M")),
+         date=as.POSIXct(paste0(date," 00:00"), format="%m/%d/%Y %H:%M")) %>%
+  group_by(date) %>%
+  summarize(m = mean(negWP, na.rm = TREU), se = SE(negWP), sd = sd(negWP))
 
 #### making figure ####
 fig1<-ggplot()+
   geom_point(data=new_d1, aes(x=datetime, y=psy))+
   geom_point(data=sum_WP, aes(x=date, y=m), stat="identity", size=3, col="red")+
-  geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
+  geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity", width=5)
 fig1
 
