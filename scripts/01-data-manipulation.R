423e34f9b066ec3841e6e7e518b996d153e98681
vpaz003 <vpaz003@fiu.edu>
2021-09-23 15:23:59 -0500

    02-functions r code

---
+++ scripts/01-data-manipulation.R
@@ -0,0 +1,8 @@
+#Manipulating data with tidyr and dyplr
+
+
+#library packages
+library(dplyr) # data manipulation functions,akin to manual filtering, calculations
+library(tidyr) # reshaping data functions 
+library(readr) # reading and writing csvs
+library(udunits2) # unit conversions
---
+++ scripts/02-functions.R
@@ -0,0 +1,1 @@
+# learninf how to write 
