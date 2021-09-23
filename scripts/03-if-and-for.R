3c915618bc28668940006eac8474f8e63c097dab
Kristina Riemer <kristina.riemer@weecology.org>
2021-09-23 14:01:24 -0700

    Add new control flow script with if material

---
+++ scripts/03-if-and-for.R
@@ -0,0 +1,50 @@
+# Learning if statements
+
+# if (the conditional statement is TRUE) {
+#   do something
+# }
+
+x <- 4
+if (x > 5) {
+  x <- x ^ 2
+}
+
+veg_type <- "tree"
+volume <- 16
+if (veg_type == "tree") {
+  mass <- 2.6 * volume ^ 0.9
+}
+
+veg_type <- "forb"
+if (veg_type == "tree") {
+  mass <- 2.6 * volume ^ 0.9
+} else if (veg_type == "grass") {
+  mass <- 0.6 * volume ^ 1.2
+} else {
+  mass <- NA
+}
+
+length <- "fjdskl"
+ifelse(length == 5, "correct", ifelse(length == 6, "correct", "incorrect"))
+
+# Exercise
+
+# Question: Complete if statement so that age_class is sapling
+# returns a y of 10, and age_class of seedling sets y to 5
+# if age_class is neither, return y of 0
+
+age_class <- "xyz"
+if(age_class == "sapling") {
+  y <- 10
+} else if (age_class == "seedling") {
+  y <- 5
+} else {
+  y <- 0
+}
+
+y <- 5
+5 -> y
+y = 5
+
+
+
