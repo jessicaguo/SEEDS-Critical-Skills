249c6f3e957b7c1ffa15317d27ef1b5eeec2a221
vpaz003 <vpaz003@fiu.edu>
2021-09-30 15:09:32 -0500

    ggplots code for class

--- scripts/03-if-and-for.R
+++ scripts/03-if-and-for.R
@@ -39,6 +39,65 @@ if (age_class== "sapling"){
   y <- 0
 }
 
+# Learning for loops - repeated actions on a list 
+ 
+#for(item in list_of_items){
+#  do_something(item)
+# }
+
+volumes <- c(1.6,3,8)
+
+for (volume in volumes) {
+  mass <- 2.65 *volume ^ 0.9
+  mass_lb <- mass *2.2
+  print(mass_lb)
+}
+
+
+for (i in 1:length(volumes)){
+  mass <- 2.65 * volumes [i]^ 0.9 
+  print(mass)
+}
+
+# used to save the data 
+# create empty object
+masses <- vector (mode = "numeric", length = length(volumes))
+
+for (i in 1:length(volumes)){
+  mass <- 2.65 * volumes [i]^ 0.9 
+  masses[i] <- mass # assign results so empty vector 
+}
+
+# Exercise 1
+# This prints numbers 1 through 5 as is, modify it to
+# print each of the numbers multiplied by 3
+for (i in 1:5){
+ thrice <- 3 * i
+  print(thrice)
+}
+
+# Exercise 2
+# Complete code to print out name of each bird
+
+birds = c("robin","woodpecker","bluejay", "sparrow")
+
+for (i in 1:length(birds)){
+  print(birds[i])
+}
+
+download.file("http://www.datacarpentry.org/semester-biology/data/locations.zip","locations.zip")
+unzip("location.zip")
+
+data_files <-  list.files (path = "locations", pattern = "locations",full.names = TRUE)
+
+results <- c()
+
+for (i in 1:length(data_files)) {
+  data <- read.csv(data_files[i])
+  count <- nrow(data)
+  results [i] <- count
+}
+
 
 
 
--- scripts/04-plots.R
+++ scripts/04-plots.R
@@ -40,22 +40,66 @@ ggplot (data = surveys_complete,
   geom_boxplot(alpha= 0, color= "blue") 
 
 # Weight - hindfoot_length correlation for each species  
-species <- surveys_complete$species_id
+sp <- species_counts$species_id
 
 
-for (i in 1:species) {
+for (i in 1:nrow(species_counts)) {
   sub <- surveys_complete %>% 
-    filter (species_id==species[i])
+    filter (species_id==sp[i])
+  
+  fig <- ggplot(data= sub,
+                mapping = aes(x = weight, y = hindfoot_length)) + 
+    geom_point(alpha= 0.1) 
+  ggsave(filename = paste0("plots/",sp[i],"_length_weight_scatter.jpg"),
+         height = 4,
+         width = 4,
+         units ="in")
+  
 }
   
 
-fig1 <- ggplot(data= surveys_complete,
-       mapping = aes(x = weight, y = hindfoot_length)) + 
-  geom_point(alpha= 0.1) 
+
+# Time Series plots 
+
+surveys_complete %>% 
+  filter(species_id=="DM") %>% 
+  group_by(year) %>% 
+  summarize( mean_weight= mean(weight),
+            sd_weight= sd(weight)) %>% 
+  ggplot(aes(x= year, y=mean_weight)) +
+  geom_pointrange(aes (ymin= mean_weight -sd_weight,
+                     ymax= mean_weight +sd_weight))
 
 
-
-
+# Challenges genus dipodomys, separate by species/ year/ sex
+ surveys_complete %>% 
+  filter(genus=="Dipodomys") %>% 
+  group_by(year,sex, species_id) %>% 
+  summarize( mean_weight= mean(weight),
+             sd_weight= sd(weight)) %>% 
+  ggplot(aes(x= year, y=mean_weight, color= sex)) +
+  geom_pointrange(aes (ymin= mean_weight -sd_weight,
+                       ymax= mean_weight +sd_weight))+ 
+   facet_wrap(vars(species_id), scales= "free_y", nrow= 3)
+  
+  
+ #Using Facet Grid and geom text 
+ 
+ surveys_complete %>%
+   filter(genus == "Dipodomys") %>%
+   group_by(year, species_id, sex) %>%
+   summarize(mean_weight = mean(weight),
+             sd_weight = sd(weight),
+             n = n()) %>%
+   ggplot(aes(x = year, y = mean_weight, color = sex)) +
+   geom_pointrange(aes(ymin = mean_weight - sd_weight,
+                       ymax = mean_weight + sd_weight), 
+                   shape = 0) +
+   geom_text(aes(label = n), size = 3, 
+             color = "black") +
+   facet_grid(rows = vars(species_id), 
+              cols = vars(sex),
+              scales = "free_y")
 
   
 
